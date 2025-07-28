1396994d45c91dbc3b36811a7a899b2750acd0f9
Daniel Rusnok <daniel.rusnok@gmail.com>
2025-07-28 19:34:13 +0200

    feat: initial local run

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Posts from './Posts';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Luminex React App</h1>
-      <Posts />
     </div>
   );
 }
