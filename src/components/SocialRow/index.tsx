aede986c0cafd1be7b703441e1f0bc59b9a26d69
Daniel Rusnok <daniel.rusnok@gmail.com>
2025-07-29 20:31:36 +0200

    feat: socials links

--- src/components/SocialRow/index.tsx
+++ src/components/SocialRow/index.tsx
@@ -4,13 +4,13 @@ import { SocialRowContainer, SocialIcon } from './styled';
 export const SocialRow: React.FC = () => {
   return (
     <SocialRowContainer>
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://facebook.com/daniel.rusnok" target="_blank" rel="noopener noreferrer">
         <SocialIcon src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" />
       </a>
-      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-        <SocialIcon src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" />
+      <a href="https://x.com/DanielRusnok" target="_blank" rel="noopener noreferrer">
+        <SocialIcon src="https://cdn-icons-png.flaticon.com/512/5968/5968958.png" alt="X" />
       </a>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://instagram.com/daniel.rusnok.cz/" target="_blank" rel="noopener noreferrer">
         <SocialIcon src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" />
       </a>
       <a href="mailto:daniel.rusnok@gmail.com" target="_blank" rel="noopener noreferrer">
