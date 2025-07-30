b7fbe998a29a362af526fdde5aa0a641f75f0b19
Daniel Rusnok <daniel.rusnok@gmail.com>
2025-07-30 10:34:04 +0200

    Add Filter component to display unique tags

---
+++ src/components/Filter/styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FilterWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  justify-content: center;
+  margin: 20px auto;
+`;
+
+export const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  font-weight: 600;
+  font-size: 18px;
+`;
