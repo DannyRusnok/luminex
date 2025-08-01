e84247c150907f3bd525cfcba3f49d7314d93904
Daniel Rusnok <daniel.rusnok@gmail.com>
2025-08-01 17:24:46 +0200

    block scrolling and hide scroll-to-top when modal open

--- src/styled.ts
+++ src/styled.ts
@@ -4,6 +4,15 @@ export const GlobalStyle = createGlobalStyle`
   html {
     scroll-behavior: smooth;
   }
+
+  body.modal-open {
+    overflow: hidden;
+    touch-action: none;
+  }
+
+  body.modal-open .scroll-to-top {
+    display: none !important;
+  }
 `;
 
 export const Layout = styled.div``;
